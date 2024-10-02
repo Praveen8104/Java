ddae2317d558aa28bab3d32ebb2616f871ab3763
Praveen8104 <praveenreddygoli8@gmail.com>
2024-10-02 14:51:38 +0530

    Refactor printArray method to improve code readability and maintainability

--- Day1.java
+++ Day1.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Day1 {
     static void printArray(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,6 +17,7 @@ public class Day1 {
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
+        System.out.print("The Given Array is : ");
         printArray(arr);
         System.out.println("The Sum of the Array is : " + sum);
         sc.close();
